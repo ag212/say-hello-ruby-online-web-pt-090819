eaf5d01e587bdb2fb6a2cc6dfe5e01c26d401f21
Abraham Galva <ag212@icloud.com>
2019-09-11 19:04:01 +0000

    Automatically backed up by Learn

--- say_hello.rb
+++ say_hello.rb
@@ -1,7 +1,7 @@
 def say_hello (name)
-  puts "Hello #{name} !"
+  puts "Hello #{name}!"
 end
 
 def greeting (name, language="Ruby")
-  puts "Hello, #{name} #{language} programmer !."
+  puts "Hello, #{name} #{language} programmer!."
 end
